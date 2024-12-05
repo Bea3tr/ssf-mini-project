b985a5b6c986df9a06bc89dc9fd5b283fad09f4c
Beatrice <beatricesim31@gmail.com>
2024-12-05 19:36:53 +0800

    spring-boot template

---
+++ src/main/java/ssf/budgetbliss/BudgetblissApplication.java
@@ -0,0 +1,13 @@
+package ssf.budgetbliss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetblissApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetblissApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ssf/budgetbliss/BudgetblissApplicationTests.java
@@ -0,0 +1,13 @@
+package ssf.budgetbliss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetblissApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
